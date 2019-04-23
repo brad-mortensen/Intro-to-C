47c61d6f23f30517e94ff6a665058018f21c3eb6
Brad Mortensen <b.mort86@gmail.com>
2019-04-23 15:33:30 -0600

    refactoring mallocs

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -12,7 +12,13 @@
 */
 char *string_dup(char *src)
 {
-
+  char *dup = malloc(sizeof *dup * (strlen(src) + 1));
+  while(*src != '\0')
+  {
+    *dup = *src;
+    dup++;
+    src++;
+  }
 }
 
 /*
@@ -24,7 +30,12 @@ char *string_dup(char *src)
 */
 void mem_copy(void *dest, const void *src, int n)
 {
-
+  char *dest2 = dest;
+  const char *src2 = src;
+  for(int i = 0; i < n; i++)
+  {
+    dest2[i] = src2[i];
+  }
 }
 
 /*
@@ -40,48 +51,54 @@ void mem_copy(void *dest, const void *src, int n)
 */
 void *resize_memory(void *ptr, int old_size, int new_size)
 {
-
+  char *ptr2 = ptr;
+  if(old_size > new_size)
+  {
+    return;
+  }
 }
 
 #ifndef TESTING
 int main(void)
 {
-    char *s = "Some string to duplicate.";
-    char *dup = string_dup(s);
+  char *s = "Some string to duplicate.";
+  char *dup = string_dup(s);
 
-    printf("Duplicated string: %s\n", dup);
+  printf("Duplicated string: %s\n", dup);
 
-    int numbers[] = {100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79};
-    int n = sizeof(numbers) / sizeof(numbers[0]);
-    int *target = malloc(n * sizeof(int));
-    
-    mem_copy(target, numbers, n * sizeof(int));
+  int numbers[] = {100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79};
+  int n = sizeof(numbers) / sizeof(numbers[0]);
+  int *target = malloc(n * sizeof(int));
 
-    printf("Copied array: ");
+  mem_copy(target, numbers, n * sizeof(int));
 
-    for (int i = 0; i < n; i++) {
-        printf("%d ", target[i]);
-    }
+  printf("Copied array: ");
 
-    printf("\n");
+  for (int i = 0; i < n; i++)
+  {
+    printf("%d ", target[i]);
+  }
 
-    char *url = string_dup("http://lambdaschool.com");
-    char *path = string_dup("/students/");
-    int url_length = string_length(url);
-    int path_length = string_length(path);
-    
-    int new_length = url_length - 1 + path_length;
-    char *new_url = resize_memory(url, url_length, new_length);
-    char *p = new_url + url_length;
+  printf("\n");
 
-    while (*path != '\0') {
-        *p = *path;
-        p++;
-        path++;
-    }
+  char *url = string_dup("http://lambdaschool.com");
+  char *path = string_dup("/students/");
+  int url_length = string_length(url);
+  int path_length = string_length(path);
 
-    printf("Full path string: %s\n", new_url);
+  int new_length = url_length - 1 + path_length;
+  char *new_url = resize_memory(url, url_length, new_length);
+  char *p = new_url + url_length;
 
-    return 0;
+  while (*path != '\0')
+  {
+    *p = *path;
+    p++;
+    path++;
+  }
+
+  printf("Full path string: %s\n", new_url);
+
+  return 0;
 }
 #endif
